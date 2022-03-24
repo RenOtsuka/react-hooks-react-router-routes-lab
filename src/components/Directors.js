b2d41950c6344d0a8c1c9fbd8f413f21ec27e165
Ryan Delos Santos <ryansantos.delossantos121@gmail.com>
2022-03-24 12:43:01 -0500

    commit tree 62a398a931be2fc19e7b1f1dc96a72aa070fb371

--- src/components/Directors.js
+++ src/components/Directors.js
@@ -2,7 +2,31 @@ import React from "react";
 import { directors } from "../data";
 
 function Directors() {
-  return <div>{/*{code here}*/}</div>;
+
+  function renderDirectorMovies(movies){
+    const directorMovieList = movies.map( movie => (
+      <li key={movie}>{movie}</li>
+    ));
+
+    return directorMovieList;
+  }
+
+  const directorList = directors.map( director => (
+    <div key={director.name}>
+      <h3>Name: {director.name}</h3>
+      Movies:
+      <ul>
+        {renderDirectorMovies(director.movies)}
+      </ul>
+    </div>
+  ));
+
+
+  return (
+  <div>
+    <h1>Directors Page</h1>
+    {directorList}
+  </div>);
 }
 
 export default Directors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,33 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+
+  function renderGenres(genres){
+    const genreList = genres.map(genre => (
+        <li key={genre}>{genre}</li>
+      )
+    );
+    return genreList;
+  }
+
+  const movieList = movies.map( movie => (
+    <div key={movie.title}>
+      <h3>Name: {movie.title}</h3>
+      Time: {movie.time}
+     <br/>
+      <p>Genres:</p>
+      <ul>
+        {renderGenres(movie.genres)}
+      </ul>
+    </div>
+  ));
+
+  return (
+  <div>
+    <h1>Movies Page</h1>
+    {movieList}
+  </div>
+  );
 }
 
 export default Movies;
